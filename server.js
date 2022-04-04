11de3f70533dc9c075a21954e9714a5daf1427d4
Shawn <shawn.m.dietrich@gmail.com>
2022-04-04 08:05:53 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//import express library
+const express = require('express');
+const app = express();
+//Import helper functions
+const { quotes } = require('./data');
+const { getRandomElement } = require('./utils');
+//Set port for server
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
